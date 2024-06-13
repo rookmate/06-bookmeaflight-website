4064f649b4abe46446a16a044b3b0aadb37d8c1f
rookmate <14072042+rookmate@users.noreply.github.com>
2024-06-13 22:18:16 +0100

    Dynamic sized icons

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import EmailIcon from "./EmailIcon";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-[100dvh]">
+    <div className="flex flex-col min-h-screen">
       <main className="flex-1">
-        <section className="w-full h-[100vh] bg-[url('https://res.cloudinary.com/dnwbkkjpo/image/upload/v1718299505/ld-export-9dddc056-06132024_jlqaft.jpg')] bg-cover bg-center">
+        <section className="w-full h-screen bg-[url('https://res.cloudinary.com/dnwbkkjpo/image/upload/v1718299505/ld-export-9dddc056-06132024_jlqaft.jpg')] bg-cover bg-center">
           <div className="w-full h-full bg-gray-900/50 flex flex-col items-center justify-center">
             <div className="text-center space-y-4 px-4 md:px-6">
               <h1 className="text-4xl font-bold text-white sm:text-5xl md:text-6xl lg:text-7xl">
@@ -18,10 +18,10 @@ export default function Home() {
             </div>
             <div className="flex items-center gap-4 mt-4"> {/* Added mt-4 for margin top */}
               <Link href="mailto:hello@bookmeaflight.eu" aria-label="Email" prefetch={false}>
-                <EmailIcon className="h-5 w-5 text-white hover:text-gray-200" />
+                <EmailIcon className="h-5 w-5 md:h-7 md:w-7 text-white hover:text-gray-200" />
               </Link>
               <Link href="https://instagram.com/bookmeaflight" aria-label="Instagram" prefetch={false}>
-                <InstagramIcon className="h-5 w-5 text-white hover:text-gray-200" />
+                <InstagramIcon className="h-5 w-5 md:h-7 md:w-7 text-white hover:text-gray-200" />
               </Link>
             </div>
           </div>
