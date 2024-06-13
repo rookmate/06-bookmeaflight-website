2a1a1b24b0fc03899f71394d88fda85524ef7237
rookmate <14072042+rookmate@users.noreply.github.com>
2024-06-13 18:58:17 +0100

    Add Hotels images

--- app/hotels/page.tsx
+++ app/hotels/page.tsx
@@ -8,7 +8,7 @@ export default function Hotels() {
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         <div className="row-span-2 overflow-hidden rounded-lg">
           <Image
-            src={`${cloudinaryBaseURL}/v1718299505/ld-export-9dddc056-06132024_jlqaft.jpg`}
+            src={`${cloudinaryBaseURL}/v1718300222/IMG_6863_jpg_s50o9d.jpg`}
             alt="Gallery Image"
             width={600}
             height={400}
@@ -17,25 +17,16 @@ export default function Hotels() {
         </div>
         <div className="overflow-hidden rounded-lg">
           <Image
-            src={`${cloudinaryBaseURL}/v1718299505/ld-export-9dddc056-06132024_jlqaft.jpg`}
+            src={`${cloudinaryBaseURL}/v1718300225/IMG_7028_jpg_lezqhf.jpg`}
             alt="Gallery Image"
             width={400}
             height={300}
             className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
           />
         </div>
-        <div className="row-span-2 overflow-hidden rounded-lg">
-          <Image
-            src={`${cloudinaryBaseURL}/v1718299505/ld-export-9dddc056-06132024_jlqaft.jpg`}
-            alt="Gallery Image"
-            width={400}
-            height={500}
-            className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
-          />
-        </div>
         <div className="overflow-hidden rounded-lg">
           <Image
-            src={`${cloudinaryBaseURL}/v1718299505/ld-export-9dddc056-06132024_jlqaft.jpg`}
+            src={`${cloudinaryBaseURL}/v1718300221/IMG_6789_wjezaz.jpg`}
             alt="Gallery Image"
             width={350}
             height={350}
@@ -44,7 +35,7 @@ export default function Hotels() {
         </div>
         <div className="overflow-hidden rounded-lg">
           <Image
-            src={`${cloudinaryBaseURL}/v1718299505/ld-export-9dddc056-06132024_jlqaft.jpg`}
+            src={`${cloudinaryBaseURL}/v1718300221/IMG_6741_jpg_jg9zzg.jpg`}
             alt="Gallery Image"
             width={400}
             height={400}
@@ -53,7 +44,7 @@ export default function Hotels() {
         </div>
         <div className="row-span-2 overflow-hidden rounded-lg">
           <Image
-            src={`${cloudinaryBaseURL}/v1718299505/ld-export-9dddc056-06132024_jlqaft.jpg`}
+            src={`${cloudinaryBaseURL}/v1718300220/IMG_6742_jpg_asrywl.jpg`}
             alt="Gallery Image"
             width={600}
             height={500}
@@ -62,7 +53,7 @@ export default function Hotels() {
         </div>
         <div className="overflow-hidden rounded-lg">
           <Image
-            src={`${cloudinaryBaseURL}/v1718299505/ld-export-9dddc056-06132024_jlqaft.jpg`}
+            src={`${cloudinaryBaseURL}/v1718300220/IMG_6748_jpg_fxwr2z.jpg`}
             alt="Gallery Image"
             width={450}
             height={300}
@@ -71,10 +62,154 @@ export default function Hotels() {
         </div>
         <div className="overflow-hidden rounded-lg">
           <Image
-            src={`${cloudinaryBaseURL}/v1718299505/ld-export-9dddc056-06132024_jlqaft.jpg`}
+            src={`${cloudinaryBaseURL}/v1718300219/IMG_6740_jpg_pbnfdf.jpg`}
             alt="Gallery Image"
             width={400}
             height={400}
+            className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
+          />
+        </div>
+        <div className="row-span-2 overflow-hidden rounded-lg">
+          <Image
+            src={`${cloudinaryBaseURL}/v1718300216/IMG_6885_jpg_ar2ueg.jpg`}
+            alt="Gallery Image"
+            width={600}
+            height={500}
+            className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
+          />
+        </div>
+        <div className="overflow-hidden rounded-lg">
+          <Image
+            src={`${cloudinaryBaseURL}/v1718300216/IMG_6772_irwp1t.jpg`}
+            alt="Gallery Image"
+            width={450}
+            height={300}
+            className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
+          />
+        </div>
+        <div className="overflow-hidden rounded-lg">
+          <Image
+            src={`${cloudinaryBaseURL}/v1718300216/IMG_6773_hj8xl8.jpg`}
+            alt="Gallery Image"
+            width={400}
+            height={400}
+            className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
+          />
+        </div>
+        <div className="overflow-hidden rounded-lg">
+          <Image
+            src={`${cloudinaryBaseURL}/v1718300214/IMG_7024_jpg_aozrtb.jpg`}
+            alt="Gallery Image"
+            width={600}
+            height={400}
+            className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
+          />
+        </div>
+        <div className="overflow-hidden rounded-lg">
+          <Image
+            src={`${cloudinaryBaseURL}/v1718300213/IMG_6796_jpg_oocpom.jpg`}
+            alt="Gallery Image"
+            width={400}
+            height={300}
+            className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
+          />
+        </div>
+        <div className="row-span-2 overflow-hidden rounded-lg">
+          <Image
+            src={`${cloudinaryBaseURL}/v1718300213/IMG_6777_thaimy.jpg`}
+            alt="Gallery Image"
+            width={400}
+            height={500}
+            className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
+          />
+        </div>
+        <div className="overflow-hidden rounded-lg">
+          <Image
+            src={`${cloudinaryBaseURL}/v1718300211/IMG_6784_wvxz6s.jpg`}
+            alt="Gallery Image"
+            width={350}
+            height={350}
+            className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
+          />
+        </div>
+        <div className="overflow-hidden rounded-lg">
+          <Image
+            src={`${cloudinaryBaseURL}/v1718300210/IMG_6781_aokbbd.jpg`}
+            alt="Gallery Image"
+            width={400}
+            height={400}
+            className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
+          />
+        </div>
+        <div className="overflow-hidden rounded-lg">
+          <Image
+            src={`${cloudinaryBaseURL}/v1718300206/IMG_6800_jpg_aqcgsq.jpg`}
+            alt="Gallery Image"
+            width={600}
+            height={400}
+            className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
+          />
+        </div>
+        <div className="overflow-hidden rounded-lg">
+          <Image
+            src={`${cloudinaryBaseURL}/v1718300206/IMG_7057_jpg_ivzs4t.jpg`}
+            alt="Gallery Image"
+            width={400}
+            height={300}
+            className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
+          />
+        </div>
+        <div className="row-span-2 overflow-hidden rounded-lg">
+          <Image
+            src={`${cloudinaryBaseURL}/v1718300206/IMG_7216_jpg_ex9j1t.jpg`}
+            alt="Gallery Image"
+            width={400}
+            height={500}
+            className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
+          />
+        </div>
+        <div className="overflow-hidden rounded-lg">
+          <Image
+            src={`${cloudinaryBaseURL}/v1718299917/IMG_7085_jpg_b7oope.jpg`}
+            alt="Gallery Image"
+            width={350}
+            height={350}
+            className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
+          />
+        </div>
+        <div className="overflow-hidden rounded-lg">
+          <Image
+            src={`${cloudinaryBaseURL}/v1718299917/IMG_7147_jpg_vzz6aj.jpg`}
+            alt="Gallery Image"
+            width={400}
+            height={400}
+            className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
+          />
+        </div>
+        <div className="overflow-hidden rounded-lg">
+          <Image
+            src={`${cloudinaryBaseURL}/v1718299917/IMG_7060_jpg_ovqysi.jpg`}
+            alt="Gallery Image"
+            width={600}
+            height={400}
+            className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
+          />
+        </div>
+        <div className="overflow-hidden rounded-lg">
+          <Image
+            src={`${cloudinaryBaseURL}/v1718299911/IMG_7225_jpg_rqj5ih.jpg`}
+            alt="Gallery Image"
+            width={400}
+            height={300}
+            className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
+          />
+        </div>
+        <div className="row-span-2 overflow-hidden rounded-lg">
+          <Image
+            src={`${cloudinaryBaseURL}/v1718299909/IMG_7171_jpg_r9zet4.jpg`}
+            alt="Gallery Image"
+            width={400}
+            height={500}
             className="h-full w-full object-cover transition-all duration-300 hover:scale-105"
           />
         </div>
