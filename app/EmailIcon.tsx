ba0102ae99d18ed0b4543c7b65904aa0657a5fdd
rookmate <14072042+rookmate@users.noreply.github.com>
2024-06-13 18:03:42 +0100

    Add icons

---
+++ app/EmailIcon.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const EmailIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M4 4h16v16H4z" />
+    <path d="M22 6l-10 7L2 6" />
+  </svg>
+);
+
+export default EmailIcon;
