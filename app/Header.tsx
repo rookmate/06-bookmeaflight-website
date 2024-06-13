80bb863c76716585310a0a9ed8a99e8ffb1426c0
rookmate <14072042+rookmate@users.noreply.github.com>
2024-06-13 18:56:38 +0100

    Reduce Header height

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,13 +3,13 @@ import PlaneIcon from "./PlaneIcon.tsx";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 w-full z-10 bg-gray-900/50 backdrop-blur-sm px-4 lg:px-6 h-14 flex items-center">
+    <header className="fixed top-0 left-0 w-full z-10 bg-gray-900/50 backdrop-blur-sm px-4 lg:px-6 h-10 flex items-center">
       <Link
         href="/"
         className="flex items-center justify-center"
         prefetch={false}
       >
-        <PlaneIcon className="h-6 w-6 text-white" />
+        <PlaneIcon className="h-5 w-5 text-white" />
         <span className="sr-only">Portfolio</span>
       </Link>
       <nav className="ml-auto flex gap-4 sm:gap-6">
