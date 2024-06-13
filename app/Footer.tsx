4c8509245789c597ab06ed92b49ab927449003ed
rookmate <14072042+rookmate@users.noreply.github.com>
2024-06-13 22:13:13 +0100

    Ensure Footer is at absolute bottom

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -4,7 +4,7 @@ import EmailIcon from "./EmailIcon";
 
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 w-full z-10 bg-gray-900/50 backdrop-blur-sm px-4 lg:px-6 h-10 flex items-center">
+    <footer className="absolute bottom-0 left-0 w-full z-10 bg-gray-900/50 backdrop-blur-sm px-4 lg:px-6 py-2">
       <div className="container mx-auto flex justify-center items-center">
         <p className="text-xs text-white text-center">
           &copy; 2024 Bookmeaflight, All rights reserved.
