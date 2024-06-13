370a1bffcaa6c8cca2e39a2333066fe6bd2c50bd
rookmate <14072042+rookmate@users.noreply.github.com>
2024-06-13 19:07:16 +0100

    Fix error: Parameter 'props' implicitly has an 'any' type

--- app/InstagramIcon.tsx
+++ app/InstagramIcon.tsx
@@ -1,4 +1,6 @@
-export default function InstagramIcon(props) {
+import React from 'react';
+
+export default function InstagramIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
--- app/PlaneIcon.tsx
+++ app/PlaneIcon.tsx
@@ -1,4 +1,6 @@
-export default function PlaneIcon(props) {
+import React from 'react';
+
+export default function PlaneIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
