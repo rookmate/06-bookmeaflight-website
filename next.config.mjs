171e3f8a0f45410651c0d167d8ae3322a85e5037
rookmate <14072042+rookmate@users.noreply.github.com>
2024-06-14 17:11:20 +0100

    Fix images.domains configuration is deprecated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https', // Match both http and https
+        hostname: 'res.cloudinary.com', // Replace with your domain
+      },
+    ],
   },
 };
 
