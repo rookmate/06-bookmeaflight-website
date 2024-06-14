6abdd46a9816162bd690ffe5160919ed86831544
rookmate <14072042+rookmate@users.noreply.github.com>
2024-06-14 12:41:42 +0100

    Fix Footer positioning

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="w-full bg-gray-900/50 backdrop-blur-sm px-4 lg:px-6 py-2">
+    <footer className="fixed bottom-0 left-0 w-full bg-gray-900/50 backdrop-blur-sm px-4 lg:px-6 py-2">
       <div className="container mx-auto flex justify-center items-center">
         <p className="text-xs text-white text-center">
           &copy; 2024 Bookmeaflight, All rights reserved.
