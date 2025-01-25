1806ef5aeb5a99a1f01b7aa82e21e01f4fcae00b
Rookmate <14072042+rookmate@users.noreply.github.com>
2025-01-25 18:22:05 +0000

    Auto-adjust quality and choose best format (WebP, AVIF)

--- app/brands/page.tsx
+++ app/brands/page.tsx
@@ -4,17 +4,16 @@ export default function Brands() {
   const cloudinaryBaseURL = "https://res.cloudinary.com/dnwbkkjpo/image/upload";
 
   const chapterData = {
-    title: "Marta Ponti",
     images: [
-      { src: `${cloudinaryBaseURL}/v1737806061/mp-mustard-purse.jpg`, alt: "Mustard purse" },
-      { src: `${cloudinaryBaseURL}/v1737806061/mp-mustard-purse2.jpg`, alt: "Mustard purse" },
-      { src: `${cloudinaryBaseURL}/v1737806061/mp-travel-bag.jpg`, alt: "Travel bag" },
-      { src: `${cloudinaryBaseURL}/v1737806061/mp-travel-bag-wallet.jpg`, alt: "Travel bag and wallet" },
-      { src: `${cloudinaryBaseURL}/v1737806061/mp-travel-bag-wallet2.jpg`, alt: "Travel bag and wallet" },
-      { src: `${cloudinaryBaseURL}/v1737806061/mp-wallet.jpg`, alt: "Wallet" },
-      { src: `${cloudinaryBaseURL}/v1737806061/mp-red-purse.jpg`, alt: "Red purse" },
-      { src: `${cloudinaryBaseURL}/v1737806061/mp-red-purse2.jpg`, alt: "Red purse" },
-      { src: `${cloudinaryBaseURL}/v1737806061/mp-red-purse3.jpg`, alt: "Red purse" },
+      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-mustard-purse.jpg`, alt: "Mustard purse" },
+      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-mustard-purse2.jpg`, alt: "Mustard purse" },
+      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-travel-bag.jpg`, alt: "Travel bag" },
+      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-travel-bag-wallet.jpg`, alt: "Travel bag and wallet" },
+      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-travel-bag-wallet2.jpg`, alt: "Travel bag and wallet" },
+      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-wallet.jpg`, alt: "Wallet" },
+      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-red-purse.jpg`, alt: "Red purse" },
+      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-red-purse2.jpg`, alt: "Red purse" },
+      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-red-purse3.jpg`, alt: "Red purse" },
     ],
   }
 
