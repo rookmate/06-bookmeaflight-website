9d18cbe1162431cfd769f5ee8802bb6ba8c5068a
Rookmate <14072042+rookmate@users.noreply.github.com>
2025-01-26 19:26:09 +0000

    Simplify URLs

--- app/brands/page.tsx
+++ app/brands/page.tsx
@@ -3,20 +3,20 @@
 import ImageChapter from "../components/ImageChapter"
 
 export default function Brands() {
-  const cloudinaryBaseURL = "https://res.cloudinary.com/dnwbkkjpo/image/upload";
+  const cloudinaryBaseURL = "https://res.cloudinary.com/dnwbkkjpo/image/upload/q_auto,f_auto";
 
   const chapterData = {
     images: [
-      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-mustard-purse.jpg`, alt: "Mustard purse" },
-      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-mustard-purse2.jpg`, alt: "Mustard purse" },
-      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-travel-bag.jpg`, alt: "Travel bag" },
-      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-travel-bag-wallet.jpg`, alt: "Travel bag and wallet" },
-      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-travel-bag-wallet2.jpg`, alt: "Travel bag and wallet" },
-      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-wallet.jpg`, alt: "Wallet" },
-      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-red-purse.jpg`, alt: "Red purse" },
-      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-red-purse2.jpg`, alt: "Red purse" },
-      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/mp-red-purse3.jpg`, alt: "Red purse" },
-      { src: `${cloudinaryBaseURL}/q_auto,f_auto/v1737806061/rings-brand.jpg`, alt: "Rings" },
+      { src: `${cloudinaryBaseURL}/v1737806061/mp-mustard-purse.jpg`, alt: "Mustard purse" },
+      { src: `${cloudinaryBaseURL}/v1737806061/mp-mustard-purse2.jpg`, alt: "Mustard purse" },
+      { src: `${cloudinaryBaseURL}/v1737806061/mp-travel-bag.jpg`, alt: "Travel bag" },
+      { src: `${cloudinaryBaseURL}/v1737806061/mp-travel-bag-wallet.jpg`, alt: "Travel bag and wallet" },
+      { src: `${cloudinaryBaseURL}/v1737806061/mp-travel-bag-wallet2.jpg`, alt: "Travel bag and wallet" },
+      { src: `${cloudinaryBaseURL}/v1737806061/mp-wallet.jpg`, alt: "Wallet" },
+      { src: `${cloudinaryBaseURL}/v1737806061/mp-red-purse.jpg`, alt: "Red purse" },
+      { src: `${cloudinaryBaseURL}/v1737806061/mp-red-purse2.jpg`, alt: "Red purse" },
+      { src: `${cloudinaryBaseURL}/v1737806061/mp-red-purse3.jpg`, alt: "Red purse" },
+      { src: `${cloudinaryBaseURL}/v1737806061/rings-brand.jpg`, alt: "Rings" },
     ],
   }
 
